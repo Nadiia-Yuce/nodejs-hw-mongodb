f1d3468bc522b6bd09dcc4bd6a19b47f1a4e15c7
Nadiia Yuce <soyka9616@gmail.com>
2024-10-26 13:06:38 +0200

    add routes and services

--- src/db/models/Contact.js
+++ src/db/models/Contact.js
@@ -1,28 +1,30 @@
 import { Schema, model } from 'mongoose';
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
   },
-  phoneNumber: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-  },
-  isFavourite: {
-    type: Boolean,
-    default: false,
-  },
-  contactType: {
-    type: String,
-    enum: ['work', 'home', 'personal'],
-    default: 'personal',
-  },
-  timestamps: true,
-});
+  { timestamps: true },
+);
 
 const ContactCollection = model('contact', contactSchema);
 
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
+import * as contactServices from './services/contacts.js';
 
 export function setupServer() {
   const app = express();
@@ -16,14 +17,37 @@ export function setupServer() {
 
   app.use(cors());
 
-  //   app.use('/', (req, res) => {
-  //     res.json({
-  //       message: 'Hello World!',
-  //     });
-  //   });
+  app.get('/contacts', async (req, res) => {
+    const data = await contactServices.getAllContacts();
+
+    res.json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const data = await contactServices.getContactById(contactId);
+
+    if (!data) {
+      return res.status(404).json({
+        status: 404,
+        message: 'Contact not found',
+      });
+    }
+
+    res.json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data,
+    });
+  });
 
   app.use('*', (req, res) => {
     res.status(404).json({
+      status: 404,
       message: 'Not found',
     });
   });
